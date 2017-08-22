88700c321825c52aa818bad1893ca5408f6a0593
zacck <coderv63@gmail.com>
2017-08-22 22:29:15 +0200

    Annotating functions with flow

---
+++ src/Functions.js
@@ -0,0 +1,12 @@
+/*@flow*/
+
+//how we annotate a function
+
+
+function add(x : number, y : number) : number  {
+	return x  + y;
+}
+
+add(2, 4);
+
+//We see above the inputs are stated as well as the expected outputed
